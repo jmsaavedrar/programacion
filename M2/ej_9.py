881a54a0125afb1ab0bc8c9835bc11f9e5fc2b88
jmsavedrar <jmsaavedrar@miuandes>
2022-04-12 17:43:35 -0400

    add ej 10-12

--- M2/ej_9.py
+++ M2/ej_9.py
@@ -4,13 +4,13 @@
 intentos = 0
 passwd="x"
 passwd1=""
-while passwd != passwd1 and intentos < 3:    
+while passwd != passwd1 and intentos < 3:    #while passwd1 vs passwd
     print('intentos : ', intentos + 1)
     hay_digito = False
     while not hay_digito:
-        passwd = input('Ingrese contraseña: ')
+        passwd = input('Ingrese contraseña: ') # while passwd válido
         i = 0
-        while i < len(passwd)  and  not hay_digito :
+        while i < len(passwd)  and  not hay_digito : # while check
             if passwd[i]>='0' and passwd[i]<='9' :
                 hay_digito = True
             else :
