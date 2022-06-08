2b9f18f83ea5c0c73f352cfb0db098bd2e56069b
jose.saavedra <jose.saavedra>
2022-06-08 11:05:06 -0400

    exm4

--- exams/examM4B4/eb4m4v1-base/cripto.py
+++ exams/examM4B4/eb4m4v1-base/cripto.py
@@ -1,7 +1,13 @@
 """
 """
 import matplotlib.pyplot as plt
+
+
 def load_file(filename):
+    """
+    Carga los datos en una lista de diccionarios
+    Para el examen solamente será necesario fecha y high
+    """
     f = open(filename)    
     lines = f.readlines()[1:]
     data = []
@@ -28,10 +34,16 @@ def reportar(data):
     for item in data:
         print(item)
 
+
 def aumentar_fecha(año, mes, dia):
+    """
+    Función para incrementar en 1 una fecha dada
+    año, mes y día son enteros
+    """
     año_s = año
     mes_s = mes
     dia_s = dia + 1    
+    #las siguientes líneas procesan la fecha en los último días del  mes
     if mes == 2 and dia ==  28 :
         dia_s = 1
         mes_s = mes + 1 
@@ -50,6 +62,9 @@ def aumentar_fecha(año, mes, dia):
     return (año_s, mes_s, dia_s)
 
 def pad_zero(val):
+    """
+    Convierte un entero (mes o día) en formato string con 2 caracteres
+    """
     string =''
     if val <10 :
         string = '0{}'.format(val)
@@ -57,7 +72,13 @@ def pad_zero(val):
         string = '{}'.format(val)
     return string
 
+
 def obtener_fechas(fecha, dias):
+    """
+    Dada una fecha de inicio y una cantidad de días
+    retorna una lista con las fechas desde fecha hasta fecha + (dias - 1)
+    
+    """
     año_mes_dia = fecha.split('-')
     año = int(año_mes_dia[0])
     mes = int(año_mes_dia[1])
@@ -75,12 +96,20 @@ def obtener_fechas(fecha, dias):
     
     
 def  obtener_high_value(data):
+    """
+    Obtiene un diccionario con los valores high indexado por fechas
+    """
     dict = {}
     for item in data:
         dict[item['fecha']] = item['high']
     return dict
     
+
 def obtener_high_values_fechas(data, fechas):
+    """
+    Obtiene una lista con los valores high para un conjunto de fechas
+    fechas es una lista de fechas
+    """
     vals = []
     dict = obtener_high_value(data)
     
@@ -93,7 +122,13 @@ def obtener_high_values_fechas(data, fechas):
             
     return vals
 
-def obtener_signo_alta_baja(data):    
+def obtener_signo_alta_baja(data) :
+    """
+    Obtiene un diccionario indexado por fechas con la variación
+    de cada fecha respecto a la fecha anterior
+    Considerando que la data está ordenada por fecha, la variación 
+    de una fila i se calcula como:  high de fila i  menos high de fila i - 1
+    """    
     dict = {}
     dict[data[0]['fecha']] = 0
     for i in range(1, len(data)) :
@@ -101,6 +136,10 @@ def obtener_signo_alta_baja(data):
     return dict
 
 def obtener_nro_alzas_bajas(data, fechas):
+    """
+    Cuenta la cantidad de fecha a la baja y a la alza
+    en un rango de fechas 
+    """
     dict = obtener_signo_alta_baja(data)
     alzas = 0
     bajas = 0
@@ -111,22 +150,21 @@ def obtener_nro_alzas_bajas(data, fechas):
             if dict[fecha] < 0 :
                 bajas += 1
     return (alzas, bajas)
+
+
+##Programa principal
 data_eth = load_file('ETH.csv')
 data_btc = load_file('BTC.csv')
 #reportar(data)
-#fechas = obtener_fechas('2013-04-29', 7)
-# vals = obtener_high_values_fechas(data, fechas)
+# descomentar para gráfico 1
+# fechas = obtener_fechas('2013-04-29', 7)
+# vals = obtener_high_values_fechas(data_btc, fechas)
 # plt.plot(fechas, vals)
-# print(fechas)
-# print(vals)
 
+#Gráfico2
 fechas = obtener_fechas('2019-01-12', 30)
-alzas, bajas = obtener_nro_alzas_bajas(data_eth, fechas)
+alzas_eth, bajas_eth = obtener_nro_alzas_bajas(data_eth, fechas)
 alzas_btc, bajas_btc = obtener_nro_alzas_bajas(data_btc, fechas)
-print('{} {}'.format(alzas, bajas))
-fig, hs = plt.subplots(1,2)
-hs[0].bar(['alzas', 'bajas'], [alzas, bajas], color = 'blue')
-hs[0].set_xlabel('ETH')
-hs[1].bar(['alzas', 'bajas'], [alzas_btc, bajas_btc], color = (1,0,0))
-hs[1].set_xlabel('BTC')
+plt.bar(['alzas-BTC', 'bajas-BTC'], [alzas_btc, bajas_btc])
+plt.bar(['alzas_ETH', 'bajas_ETH'], [alzas_eth, bajas_eth], color = 'red')
 plt.show()
